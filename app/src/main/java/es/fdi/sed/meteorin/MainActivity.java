5c4603bddaff758696f813f4288a895ad681e377
Sorin <sorindra@ucm.es>
2021-04-17 20:25:03 +0200

    Added a "last updated" text to the main activity, to indicate the last moment the information was updated.

--- app/src/main/java/es/fdi/sed/meteorin/MainActivity.java
+++ app/src/main/java/es/fdi/sed/meteorin/MainActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.widget.TextView;
 import com.timqi.sectorprogressview.ColorfulRingProgressView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity
 {
     private ColorfulRingProgressView _temperatureGauge, _humidityGauge;
-    private TextView _temperatureText, _humidityText;
+    private TextView _temperatureText, _humidityText, _lastUpdatedText;
+    private SimpleDateFormat _dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,6 +25,11 @@ public class MainActivity extends AppCompatActivity
         _temperatureText = findViewById(R.id.TemperatureText);
         _humidityGauge = findViewById(R.id.HumidityGauge);
         _humidityText = findViewById(R.id.HumidityText);
+        _lastUpdatedText = findViewById(R.id.LastUpdatedText);
+
+        _lastUpdatedText.setText(String.format(getResources().getString(R.string.last_updated_text), "never"));
+
+        _dateFormatter = new SimpleDateFormat("HH:mm:ss", Locale.US);
 
         new DataReader(this);
     }
@@ -47,5 +57,9 @@ public class MainActivity extends AppCompatActivity
 
         _temperatureText.setText(temperatureText);
         _humidityText.setText(humidityText);
+
+        String lastUpdated = String.format(getResources().getString(R.string.last_updated_text), _dateFormatter.format(new Date()));
+
+        _lastUpdatedText.setText(lastUpdated);
     }
 }
