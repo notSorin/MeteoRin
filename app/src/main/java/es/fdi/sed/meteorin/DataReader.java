7b90cf74d2582880cd633d491ba7eb158c10b4b1
Sorin <sorindra@ucm.es>
2021-04-17 21:07:30 +0200

    Code formatting.

--- app/src/main/java/es/fdi/sed/meteorin/DataReader.java
+++ app/src/main/java/es/fdi/sed/meteorin/DataReader.java
@@ -1,5 +1,6 @@
 package es.fdi.sed.meteorin;
 
+import android.annotation.SuppressLint;
 import com.harrysoft.androidbluetoothserial.BluetoothManager;
 import com.harrysoft.androidbluetoothserial.BluetoothSerialDevice;
 import com.harrysoft.androidbluetoothserial.SimpleBluetoothDeviceInterface;
@@ -10,41 +11,39 @@ public class DataReader
 {
     private final MainActivity MAIN_ACTIVITY;
     private final String HC06_MAC = "00:14:03:05:59:38";
-    private SimpleBluetoothDeviceInterface deviceInterface;
+    private SimpleBluetoothDeviceInterface _deviceInterface;
 
+    @SuppressLint("CheckResult")
     public DataReader(MainActivity ma)
     {
         MAIN_ACTIVITY = ma;
 
         BluetoothManager bluetoothManager = BluetoothManager.getInstance();
+
         bluetoothManager.openSerialDevice(HC06_MAC)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::onConnected, this::onError);
     }
 
+    // You are now connected to this device!
     private void onConnected(BluetoothSerialDevice connectedDevice)
     {
-        // You are now connected to this device!
         // Here you may want to retain an instance to your device:
-        deviceInterface = connectedDevice.toSimpleDeviceInterface();
+        _deviceInterface = connectedDevice.toSimpleDeviceInterface();
 
         // Listen to bluetooth events
-        deviceInterface.setListeners(this::onMessageReceived, this::onMessageSent, this::onError);
-
-        // Let's send a message:
-        //deviceInterface.sendMessage("Hello world!");
+        _deviceInterface.setListeners(this::onMessageReceived, this::onMessageSent, this::onError);
     }
 
+    // We sent a message! Handle it here.
     private void onMessageSent(String message)
     {
-        // We sent a message! Handle it here.
-        //Toast.makeText(context, "Sent a message! Message was: " + message, Toast.LENGTH_LONG).show(); // Replace context with your context instance.
     }
 
+    // We received a message! Handle it here.
     private void onMessageReceived(String message)
     {
-        // We received a message! Handle it here.
         String[] splits = message.split("x");
 
         if(splits.length == 2)
